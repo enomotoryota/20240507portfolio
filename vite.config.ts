2f445305473a81df5d731a37628dc4b5c24b7da9
enomotoryota <enomoito@gmail.com>
2024-05-07 16:10:55 +0900

    いけー

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/20240507portfolio/",
+  base: process.env.GITHUB_PAGES // この行を追加
+    ? "20240507portfolio" // この行を追加
+    : "./",
   plugins: [react()],
 });
