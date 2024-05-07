939f603253e0e14dc470ebcd3d4e812f486f1a50
enomotoryota <enomoito@gmail.com>
2024-05-07 16:55:06 +0900

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES // この行を追加
-    ? "20240507portfolio" // この行を追加
-    : "./",
+  base: "/20240507portfolio/",
   plugins: [react()],
 });
